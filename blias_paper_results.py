2d3eadeb585241e7ba31b453f615064cd8fdb3e3
Allen Mathew <mailtoallenmathew@gmail.com>
2026-01-25 22:20:19 -0500

    DME edits

--- blias_paper_results.py
+++ blias_paper_results.py
@@ -3,7 +3,7 @@ import qutip as qt
 import matplotlib.pyplot as plt
 
 # ====================
-# Parameters
+# Problem Parameters (h-bar = 1)
 # ====================
 N = 15  # number of cavity fock states
 wc = 1.0 * 2 * np.pi  # cavity frequency
@@ -16,26 +16,16 @@ use_rwa = False
 
 tlist = np.linspace(0, 40, 200)
 
-
-# ====================
-# Operators
-# ====================
 a  = qt.tensor(qt.destroy(N), qt.qeye(2))
 sm = qt.tensor(qt.qeye(N), qt.destroy(2))
 sz = qt.tensor(qt.qeye(N), qt.sigmaz())
 
-# ====================
-# Initial state
-# ====================
 psi0 = qt.tensor(qt.basis(N, 0), qt.basis(2, 0))
 
-# ====================
-# Full Rabi Hamiltonian
-# ====================
-H = (wc * a.dag() * a + wa / 2 * sz + g * (a + a.dag()) * (sm + sm.dag()))
+H = (wc * a.dag() * a + wa / 2 * sz + g * (a + a.dag()) * (sm + sm.dag())) # Rabi Hamiltonian
 
 # ====================
-# STANDARD master equation
+# SME
 # ====================
 c_ops_std = []
 c_ops_std.append(np.sqrt(kappa) * a)
@@ -44,9 +34,9 @@ c_ops_std.append(np.sqrt(gamma) * sm)
 out_std = qt.mesolve(H, psi0, tlist, c_ops_std, [a.dag() * a, sm.dag() * sm])
 
 # ====================
-# DRESSED master equation
+# DME
 # ====================
-evals, evecs = H.eigenstates()
+evals, estates = H.eigenstates()
 
 X_cav  = a + a.dag()
 X_atom = sm + sm.dag()
@@ -56,45 +46,30 @@ c_ops_dressed = []
 for i in range(len(evals)):
     for j in range(len(evals)):
         if evals[j] > evals[i]:
-            # Transition frequency
-            w_ij = evals[j] - evals[i]
 
-            cav_ij  = evecs[i].dag() * X_cav  * evecs[j]
-            atom_ij = evecs[i].dag() * X_atom * evecs[j]
+            cav_ij  = estates[i].dag() * X_cav  * estates[j]
+            atom_ij = estates[i].dag() * X_atom * estates[j]
 
-
-            if abs(cav_ij) > 1e-6:
-                c_ops_dressed.append(
-                    np.sqrt(kappa * abs(cav_ij)**2)
-                    * (evecs[i] * evecs[j].dag())
-                )
-
-            if abs(atom_ij) > 1e-6:
-                c_ops_dressed.append(
-                    np.sqrt(gamma * abs(atom_ij)**2)
-                    * (evecs[i] * evecs[j].dag())
-                )
+            c_ops_dressed.append(np.sqrt(kappa * abs(cav_ij)**2) * (estates[i] * estates[j].dag()))
+            c_ops_dressed.append(np.sqrt(gamma * abs(atom_ij)**2) * (estates[i] * estates[j].dag()))
 
 out_dressed = qt.mesolve(H, psi0, tlist, c_ops_dressed, [a.dag() * a, sm.dag() * sm])
 
+# ====================
+# Plotting Everything
+# ====================
 fig, axes = plt.subplots(1, 2, figsize=(14, 5), sharey=True)
 
-# ====================
-# Standard ME plot
-# ====================
 axes[0].plot(tlist, out_std.expect[0], "b--", label=r"$\langle a^\dagger a \rangle$")
 axes[0].plot(tlist, out_std.expect[1], "r--", label=r"$\langle \sigma_+\sigma_- \rangle$")
-axes[0].set_title("Standard master equation (bare operators)")
+axes[0].set_title("Standard Master Equation")
 axes[0].set_xlabel("Time")
-axes[0].set_ylabel("Occupation")
+axes[0].set_ylabel("Occupation Probability")
 axes[0].legend()
 
-# ====================
-# Dressed ME plot
-# ====================
 axes[1].plot(tlist, out_dressed.expect[0], "b", label=r"$\langle a^\dagger a \rangle$")
 axes[1].plot(tlist, out_dressed.expect[1], "r", label=r"$\langle \sigma_+\sigma_- \rangle$")
-axes[1].set_title("Dressed master equation (eigenbasis)")
+axes[1].set_title("Dressed Master Equation")
 axes[1].set_xlabel("Time")
 axes[1].legend()
 
